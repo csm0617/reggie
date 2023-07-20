f1ccc66a979036f3c870f1411dc531652b37016a
‘陈水明’ <439752552@qq.com>
2023-07-20 14:53:39 +0800

    添加对象映射器和WebMvcConfig中加入消息转换器，该项目的前端js会将后端返回的Long类型丢失精度，所以将Long类型的字段转换成String，并将后端返回的时间格式化

--- src/main/java/com/csm/reggie/config/WebMvcConfig.java
+++ src/main/java/com/csm/reggie/config/WebMvcConfig.java
@@ -1,9 +1,15 @@
 package com.csm.reggie.config;
 
+import com.csm.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
 @Slf4j
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
@@ -12,5 +18,21 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("开始加载静态资源");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/frond/**").addResourceLocations("classpath:/frond/");
+    }
+
+    /**
+     * 扩展mvc框架的消息转换器
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("消息转换器启动中...");
+        //创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter= new MappingJackson2HttpMessageConverter();
+        //设置对象转换器，底层使用Jackson,将Java对象转换为Json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        //将上面的消息转换器对象追加到mvc框架转换器的集合中，0表示转换器加入到集合最前的位置，优先使用
+        converters.add(0,messageConverter);
+
     }
 }
