d0fd2a6ee23a8f9b45d73a93ab350730831ef3ec
‘陈水明’ <439752552@qq.com>
2023-07-20 09:05:16 +0800

    在mybatisPlusConfig中新增mybatisPlus的分页控制器

---
+++ src/main/java/com/csm/reggie/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.csm.reggie.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author 快乐小柴
+ * @Date 2022/10/20 9:31
+ * @Version 1.0
+ */
+@Configuration
+@MapperScan("com.csm.reggie.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
