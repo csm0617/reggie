bd811805f8e0a224c5791666d9d9e351ab4918f4
‘陈水明’ <439752552@qq.com>
2023-07-20 16:24:16 +0800

    CategoryController加上请求的根路径和@Autowried注入service

--- src/main/java/com/csm/reggie/controller/CategoryController.java
+++ src/main/java/com/csm/reggie/controller/CategoryController.java
@@ -1,9 +1,16 @@
 package com.csm.reggie.controller;
 
+import com.csm.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequestMapping("/category")
 public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
 }
